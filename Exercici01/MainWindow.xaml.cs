4e640836df9efe0835197133cd63e239e2354909
marc <mflores.dam@institutcampalans.net>
2023-10-12 00:32:05 +0200

    pujada de la practica

---
+++ Exercici01/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercici01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void A8_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat la torre negra en la posició A8 i no et pots moure");
+        }
+
+
+        private void B8_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat caball negre en la posició B8 i es pot moure a la posició A6 i C6");
+        }
+
+        private void C8_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el alfil negre en la posició C8 i no et pots moure");
+        }
+
+        private void D8_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat la reina negre en la posició D8 i no et pots moure");
+        }
+
+        private void E8_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el rey negre en la posició E8 i no et pots moure");
+        }
+
+        private void F8_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el alfil negre en la posició F8 i no et pots moure");
+        }
+
+        private void G8_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat caball negre en la posició G8 i es pot moure a la posició F6 i H6");
+        }
+
+        private void H8_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat la torre negra en la posició H8 i no et pots moure");
+        }
+        private void A7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo negre en la posició A7 i es pot moure a la posició A6 i A5");
+        }
+
+        private void B7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo negre en la posició B7 i es pot moure a la posició B6 i B5");
+        }
+
+        private void C7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo negre en la posició C7 i es pot moure a la posició C6 i C5");
+        }
+
+        private void D7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo negre en la posició D7 i es pot moure a la posició D6 i D5");
+        }
+
+        private void E7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo negre en la posició E7 i es pot moure a la posició E6 i E5");
+        }
+
+        private void F7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo negre en la posició F7 i es pot moure a la posició F6 i F5");
+        }
+
+        private void G7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo negre en la posició G7 i es pot moure a la posició G6 i G5");
+        }
+
+        private void H7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo negre en la posició H7 i es pot moure a la posició H6 i H5");
+        }
+
+        private void A2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo blanc en la posició A2 i es pot moure a la posició A3 i A4");
+        }
+
+        private void B2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo blanc en la posició B2 i es pot moure a la posició B3 i B4");
+        }
+
+        private void C2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo blanc en la posició C2 i es pot moure a la posició C3 i C4");
+        }
+
+        private void D2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo blanc en la posició D2 i es pot moure a la posició D3 i D4");
+        }
+
+        private void E2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo blanc en la posició E2 i es pot moure a la posició E3 i E4");
+        }
+
+        private void F2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo blanc en la posició F2 i es pot moure a la posició F3 i F4");
+        }
+
+        private void G2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo blanc en la posició G2 i es pot moure a la posició G3 i G4");
+        }
+
+        private void H2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el peo blanc en la posició H2 i es pot moure a la posició H3 i H4");
+        }
+
+        private void A1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat la torre blanca en la posició A1 i no et pots moure");
+        }
+
+        private void B1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat caball blanc en la posició B1 i es pot moure a la posició A3 i C3");
+        }
+
+        private void C1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el alfil blanc en la posició C1 i no et pots moure");
+        }
+
+        private void D1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat la reina blanca en la posició D1 i no et pots moure");
+        }
+
+        private void E1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el rey blanc en la posició E1 i no et pots moure");
+        }
+
+        private void F1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat el alfil blanc en la posició F1 i no et pots moure");
+        }
+
+        private void G1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat caball blanc en la posició G1 i es pot moure a la posició H3 i F3");
+        }
+
+        private void H1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionat la torre blanca en la posició H1 i no et pots moure");
+        }
+    }
+}
